edfbe599b04ded110aedb201834afe8543092e24
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-08-12 17:36:31 +0300

    getSearchMovies(searchMovie)

--- src/api/themoviedb-api.js
+++ src/api/themoviedb-api.js
@@ -6,10 +6,20 @@ const API_KEY = '7636cf9d603a531a6043a00a518eaa9c';
 export const getTrandingMovies = () => {
   return axios
     .get('trending/movie/day?', {
-      params: {
+        params: {
         api_key: API_KEY,
       },
     })
-        .then(response => response.data)
-    
+        .then(response => response.data) 
+};
+
+export const getSearchMovies = (searchMovie) => {
+  return axios
+      .get('search/movie?', {
+          params: {
+        query: searchMovie,
+        api_key: API_KEY,
+      },
+    })
+        .then(response => response.data) 
 };
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,22 +1,62 @@
 import PageHeading from '../components/PageHeading/PageHeading';
+import { useState, useEffect } from 'react';
+import { getSearchMovies } from '../api/themoviedb-api';
 
 export default function MoviesPage() {
+  const [query, setQuery] = useState('');
+  const [searchMovie, setSearchMovie] = useState('Мішуня');
+  const [searchMovieResult, setSearchMovieResult] = useState([]);
+
+  const handleNameChange = event => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (query.trim() === '') {
+      alert('Введите запрос.');
+      return;
+    }
+
+    setSearchMovie(query);
+    setQuery('');
+  };
+
+  useEffect(() => {
+    getSearchMovies(searchMovie).then(({ results }) =>
+      setSearchMovieResult(results)
+    );
+  }, [searchMovie]);
+
   return (
     <>
       <PageHeading text="Movies" />
 
-      <form className="SearchForm">
+      <form className="SearchForm" onSubmit={handleSubmit}>
         <input
           className="SearchForm-input"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search movies"
+          onChange={handleNameChange}
+          value={query}
         />
         <button type="submit" className="SearchForm-button">
           <span style={{ fontSize: 30 }}>&#9906;</span>
         </button>
       </form>
+
+      
+
+      {searchMovie !== 'Мішуня' && (
+        <ul>
+          {searchMovieResult.map(el => (
+            <li key={el.id}>{el.title}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
