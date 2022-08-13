b723b11ec16e57974704e5919cbd92be31f9ab77
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-08-13 15:59:39 +0300

    flex

--- src/components/MoviesDetails/MoviesDetails.jsx
+++ src/components/MoviesDetails/MoviesDetails.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import { getMovieById } from '../../api/themoviedb-api';
 import { Link, Outlet } from "react-router-dom";
+import s from './MovieDetails.module.css';
 
 export default function MoviesDetails() {
   const [idMovie, setIdMovie] = useState({});
@@ -11,15 +12,18 @@ export default function MoviesDetails() {
     getMovieById(id).then(setIdMovie)
   }, [id]);
     
-    return <>{idMovie && <>
-        <img src="" alt="" />
-        <h2>{idMovie.title} </h2>
+  return <>{idMovie && <>
+      <div className={s.moviePage}>
+      <img src="https://www.themoviedb.org/t/p/original/vUUqzWa2LnHIVqkaKVlVGkVcZIW.jpg" alt={idMovie.title} width="250" />
+      <div className={s.movieDiscription}>
+        <h1>{idMovie.title} </h1>
         <h5>{idMovie.release_date} </h5>
         <h4>Overview</h4>
         <p>{idMovie.overview}</p>
         <h4>Genres</h4>
         <p>{idMovie.vote_average}</p>
-
+        </div>
+    </div>
 
 
         <h4>Additional information</h4>
